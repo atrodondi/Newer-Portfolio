0b465e9e11912ffdd18ce52d5fd614a6c3dd1a4a
Cheebachong <atrodondi@gmail.com>
2020-05-20 22:13:15 -0700

    working on a new portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$("#contact").on("click", function () {
+  $("#contactModal").modal("toggle");
+});
+
+$("#fridFoodtoggle").on("click", function () {
+  $("#fridFoodModal").modal("toggle");
+});
+
+$("#footballToggle").on("click", function () {
+  $("#footballModal").modal("toggle");
+});
+
+$("#thisdaytoggle").on("click", function () {
+  $("#thisdayModal").modal("toggle");
+});
